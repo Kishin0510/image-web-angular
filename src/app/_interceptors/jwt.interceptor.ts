6a2deebdbf9bb03cb822d04e78e32381c4e71340
Kishin520 <nicokishins@hotmail.com>
2025-01-08 19:30:25 -0300

    fix: El interceptor maneja los jwt expirados

--- src/app/_interceptors/jwt.interceptor.ts
+++ src/app/_interceptors/jwt.interceptor.ts
@@ -1,10 +1,14 @@
 import { LocalStorageService } from './../_services/local-storage.service';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
   const localStorageService = inject(LocalStorageService);
   const token = localStorageService.getVariable('token');
+  const router = inject(Router);
 
   if (token) {
     req = req.clone({
@@ -13,5 +17,15 @@ export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
       },
     });
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        localStorageService.removeVariable('token');
+        localStorageService.removeVariable('email');
+        localStorageService.updateLoginStatus(false);
+        router.navigate(['/login']);
+      }
+      return throwError(error);
+    })
+  );
 };
